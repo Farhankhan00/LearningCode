9b5c09ad88a232d36b42ac045b8e634b559b1683
Farhan Khan <farhannaveed6@gmail.com>
2019-01-13 20:37:22 -0500

    masters femeral ports

--- terraform/concourse-frontend.tf
+++ terraform/concourse-frontend.tf
@@ -26,6 +26,13 @@ resource "aws_security_group" "concourse-master" {
     ]
   }
 
+  ingress {
+    from_port = 1024
+    to_port   = 65535
+    protocol  = "tcp"
+    self = true
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
